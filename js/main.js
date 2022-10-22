9ba076bb52e6f35e8ec4fb1bfd3d85464e21025f
NoTreblee <42041720+NoTreblee@users.noreply.github.com>
2022-10-22 20:21:23 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
 anime({
   targets: '.square',
-  opacity: 1,
-  translateY: anime.stagger(-20, { grid: [15, 5], from: 'center', axis: 'y' }),
-  translateX: anime.stagger(-120, { grid: [15, 5], from: 'center', axis: 'x' }),
-  rotateZ: anime.stagger([0, 90,], { grid: [15, 5], from: 'center', axis: 'x' }),
-  rotateX: anime.stagger([70, 140,], { grid: [15, 5], from: 'center', axis: 'y' }),
-  delay: anime.stagger(200, { grid: [15, 5], from: 'center' }),
+  opacity: 0.7,
+  translateY: anime.stagger(-100, { grid: [15, 5], from: 'center', axis: 'y' }),
+  translateX: anime.stagger(20, { grid: [15, 5], from: 'center', axis: 'x' }),
+  // rotateX: anime.stagger(120, { grid: [15, 5], from: 'center', axis: 'x' }),
+  rotateZ: {
+    value: anime.stagger([90, 180, 360], {
+      grid: [15, 5],
+      from: 'center',
+      axis: 'Z',
+    }),
+  },
+  delay: anime.stagger(70, { grid: [100, 50], from: 'center' }),
   loop: true,
   direction: 'alternate',
-  duration: 6000,
+  duration: 2000,
   easing: 'easeInOutQuad',
-
-  rotate: {
-    value: anime.stagger(1),
-    delay: anime.stagger(1),
-    loop: true,
-    easing: 'easeInOutQuad',
-    duration: 12000,
-    loop: true,
-  },
+  // rotate: {
+  //   value: anime.stagger(1),
+  //   delay: anime.stagger(1),
+  //   loop: true,
+  //   easing: 'easeInOutQuad',
+  //   duration: 12000,
+  //   loop: true,
+  // },
 });
